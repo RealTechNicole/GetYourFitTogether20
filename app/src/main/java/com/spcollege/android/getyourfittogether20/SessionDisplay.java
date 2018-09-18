7294f28a60c5ca478f509c8cc4b3401aaa993102
RealTechNicole <nicoleleighpost@gmail.com>
2018-09-18 00:12:01 -0400

    Initial commit

---
+++ app/src/main/java/com/spcollege/android/getyourfittogether20/SessionDisplay.java
@@ -0,0 +1,22 @@
+package com.spcollege.android.getyourfittogether20;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class SessionDisplay extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_session_display);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+
+    }
+
+}
